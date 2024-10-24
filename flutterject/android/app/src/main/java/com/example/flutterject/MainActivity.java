39da542986adcec895419baa9ab642bb5943892c
MD. SANAULLAH <158471899+mdsanaullah2030@users.noreply.github.com>
2024-10-24 12:17:17 +0600

    aa

---
+++ flutterject/android/app/src/main/java/com/example/flutterject/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutterject;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutterject/lib/page/loginpage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class Loginpage extends StatelessWidget {
+
+final TextEditingController email=TextEditingController();
+final TextEditingController password=TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: email,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+                prefixIcon:Icon(Icons.email)
+                    
+              ),
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+
+            TextField(
+              controller: password,
+              decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  prefixIcon:Icon(Icons.password)
+
+              ),
+              obscureText: true,
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+
+            ElevatedButton(onPressed: (){
+              String em=email.text;
+              String pass=password.text;
+              print('Email:$em,Password:$pass');
+
+
+            },
+            child: Text("Login",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontFamily:GoogleFonts.lato().fontFamily
+            ),
+            ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                foregroundColor: Colors.white,
+
+
+              ),
+            )
+          ],
+        ),
+        
+      ),
+    );
+  }
+}
