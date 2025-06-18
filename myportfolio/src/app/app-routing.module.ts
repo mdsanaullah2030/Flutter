876d180ebcd5d0a74a4f801259c4679f4f806b28
MD. SANAULLAH <158471899+mdsanaullah2030@users.noreply.github.com>
2025-06-18 15:12:09 +0600

    aaa

--- myportfolio/src/app/app-routing.module.ts
+++ myportfolio/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeviewComponent } from './home/homeview/homeview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+ { path: 'about', component: HomeviewComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
